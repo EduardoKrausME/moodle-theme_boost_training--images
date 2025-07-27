cdd144c13ca7c305a6a20b7d5b25a40fe55e4cf8
Eduardo Kraus <kraus@eduardokraus.com>
2025-07-27 19:10:50 -0300

    Comments

--- create-json.php
+++ create-json.php
@@ -4,17 +4,17 @@
  * Lista todos os arquivos PNG no diretório atual e salva em um JSON.
  */
 
-// Pega todos os arquivos com extensão .png (case insensitive)
+// Get all files with .png extension (case insensitive)
 $files = array_merge(
-    glob( '*.png', GLOB_BRACE),
-    glob( '*/*.png', GLOB_BRACE),
-    glob( '*/*/*.png', GLOB_BRACE),
+    glob('*.png', GLOB_BRACE),
+    glob('*/*.png', GLOB_BRACE),
+    glob('*/*/*.png', GLOB_BRACE),
 );
 
 $jsonContent = json_encode($files, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
-// Salva no arquivo images.json
+// Save to images.json file
 file_put_contents('images.json', $jsonContent);
 
-// Saída para confirmação
-echo "Arquivo images.json criado com sucesso com " . count($files) . " imagens PNG.\n";
+// Output for confirmation
+echo count($files) . " images.\n";
